a672b19e01cc1e52fc3d3e79cd4e9e58fde51014
Niteshkumar <36584997+niteshkumar2000@users.noreply.github.com>
2020-05-14 18:33:15 +0530

    sdm660-common: Disable RRO
    Signed-off-by: Niteshkumar <nitesh156200@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -30,7 +30,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
+# PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
     $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
